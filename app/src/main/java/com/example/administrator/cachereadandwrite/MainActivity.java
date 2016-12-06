bb4169e6528ea75852839b205c4377110d2e07fa
mahui <mahui@magicalworld.cn>
2016-12-06 20:16:52 +0800

    数据整合

--- app/src/main/java/com/example/administrator/cachereadandwrite/MainActivity.java
+++ app/src/main/java/com/example/administrator/cachereadandwrite/MainActivity.java
@@ -16,6 +16,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
     TextView show;
@@ -27,40 +28,47 @@ public class MainActivity extends AppCompatActivity {
         show= (TextView) findViewById(R.id.show);
     }
 
-    /**
+    /***
      * 序列化对象
-     *
      * @param person
      * @return
-     * @throws IOException
      */
-    private String serialize(Person person) throws IOException {
+    private String serialize(Person person){
+        String serStr=null;
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-        objectOutputStream.writeObject(person);
-        String serStr = byteArrayOutputStream.toString("ISO-8859-1");
-        serStr = java.net.URLEncoder.encode(serStr, "UTF-8");
-        objectOutputStream.close();
-        byteArrayOutputStream.close();
+        ObjectOutputStream objectOutputStream = null;
+        try {
+            objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(person);
+            serStr = byteArrayOutputStream.toString("ISO-8859-1");
+            serStr = java.net.URLEncoder.encode(serStr, "UTF-8");
+            objectOutputStream.close();
+            byteArrayOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return serStr;
     }
 
     /**
      * 反序列化对象
-     *
      * @param str
      * @return
-     * @throws IOException
-     * @throws ClassNotFoundException
      */
-    private Person deSerialization(String str) throws IOException,
-            ClassNotFoundException {
-        String redStr = java.net.URLDecoder.decode(str, "UTF-8");
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(redStr.getBytes("ISO-8859-1"));
-        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-        Person person = (Person) objectInputStream.readObject();
-        objectInputStream.close();
-        byteArrayInputStream.close();
+    private Person deSerialization(String str){
+        String redStr = null;
+        Person person=null;
+        try {
+            redStr = java.net.URLDecoder.decode(str, "UTF-8");
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(redStr.getBytes("ISO-8859-1"));
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            person = (Person) objectInputStream.readObject();
+            objectInputStream.close();
+            byteArrayInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return person;
     }
 
@@ -79,19 +87,11 @@ public class MainActivity extends AppCompatActivity {
     public void onclick(View v) {
         switch (v.getId()) {
             case R.id.button1:
-                try {
-                    saveObject(serialize(new Person("小白",20,"朝阳")));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                saveObject(serialize(new Person("小白",20,"朝阳")));
                 break;
             case R.id.button2:
-                try {
-                    Person person = deSerialization(getObject());
-                    show.setText(person.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                Person person = deSerialization(getObject());
+                show.setText(person.toString());
                 break;
             default:
                 break;
